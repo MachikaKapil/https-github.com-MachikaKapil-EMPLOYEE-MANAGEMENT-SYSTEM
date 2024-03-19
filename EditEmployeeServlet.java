f3cea5216d4b5c68be2aa0b507c1f868d14fbfe5
MachikaKapil <162136385+MachikaKapil@users.noreply.github.com>
2024-03-19 22:15:39 +0530

    Add files via upload

---
+++ EditEmployeeServlet.java
@@ -0,0 +1,36 @@
+package test;
+import java.io.*;
+import java.util.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+@SuppressWarnings("serial")
+@WebServlet("/edit")
+public class EditEmployeeServlet extends HttpServlet
+{
+	@Override
+protected void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException
+{
+	HttpSession hs = req.getSession(false);
+	if(hs==null)
+	{
+		req.setAttribute("msg","Session Expired...<br>");
+		req.getRequestDispatcher("msg.jsp").forward(req, res);
+	}
+	else
+	{
+		String eId = req.getParameter("eid");
+		@SuppressWarnings("unchecked")
+		ArrayList<EmployeeBean> al = (ArrayList<EmployeeBean>)hs.getAttribute("alist");
+		Iterator<EmployeeBean> it = al.iterator();
+		while(it.hasNext())
+		{
+			EmployeeBean eb = (EmployeeBean)it.next();
+			if(eId.equals(eb.geteId()))
+				req.setAttribute("ebean", eb);
+			break;
+		}//end of loop
+		req.getRequestDispatcher("EditEmployee.jsp").forward(req, res);
+	}//end of else
+}
+}
